7d1e4d8e130d8132af832dc2bff516a4c9fab116
sreevani8 <sreevanidumpala@gmail.com>
2025-02-28 12:57:37 +0530

    this is advanced patterns first commit

---
+++ AdvPatterns/src/com/mulit/advfaqs/Test.java
@@ -0,0 +1,10 @@
+package com.mulit.advfaqs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my test program");
+	}
+
+}
